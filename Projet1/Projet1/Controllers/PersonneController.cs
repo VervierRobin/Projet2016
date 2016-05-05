4d2615e9f7db02fe1a3c9628979be0a4b03dbf9b
VervierRobin <robinvervier@hotmail.com>
2016-05-05 21:17:42 +0200

    Change Skin

---
+++ Projet1/Projet1/Controllers/PersonneController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+
+namespace Projet1.Controllers
+{
+    public class PersonneController : Controller
+    {
+        // GET: Personne
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ Projet1/Projet1/Models/Personne.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Projet1.Models
+{
+    public class Personne
+    {
+        public class AjoutPeronneViewModel
+        {
+            public string Nom { get; set; }
+            public string Prenom { get; set; }
+        }
+    }
+}
